5b10f10fec4917b16c6e8717d82fc4978284f77f
salmakhemiri <salma.khemiri@esprit.tn>
2023-10-08 23:26:42 +0100

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginRegisterComponent } from './login-register/login-register.component';
 import { MatchComponent } from './match/match.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { ApropsComponent } from './aprops/aprops.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -55,6 +55,7 @@ import { RecaptchaModule } from "ng-recaptcha";
     MatButtonModule,
     MatMenuModule,
     MatFormFieldModule,
+  
    
     RecaptchaModule,
     MatInputModule,
