3b00583db7470d5fa4bc5de1d3d3b659e93c2012
salmakhemiri <salma.khemiri@esprit.tn>
2023-10-23 12:21:22 +0100

    sirine blog/reclamation

--- src/app/add-reclamation/add-reclamation.component.ts
+++ src/app/add-reclamation/add-reclamation.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CrmService } from '../services/crm.service';
+import { ContactUs } from '../model/ContactUs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-reclamation',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-reclamation.component.css']
 })
 export class AddReclamationComponent {
+  constructor(private crmService: CrmService, private router: Router){}
+  contactUs: ContactUs = {
+    Firstname:'',
+    LastName: '',
+    Email: '',
+    contenu:'',
+    Location:''
+
+  }
+  createContactUs(f:any){
+    this.crmService.createContact(f).subscribe((response) => {
+        console.log("message envoyé");
+        this.router.navigate([''])
+      },
+      (error)=> {
+        console.log("message non envoyé");
+      })
+  }
 
 }
